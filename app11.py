70eac073aee87f9e361a85a6905ffb4f230e0867
BishanSingh246 <112897152+BishanSingh246@users.noreply.github.com>
2023-09-13 18:27:07 +0530

    Add files via upload

---
+++ app11.py
@@ -0,0 +1,33 @@
+import torch
+from TTS.api import TTS
+
+# Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+
+speakers_file_path = "hi/fastpitch/speakers.pth"
+
+text_arr = [
+"संवाद और सहयोग समस्याओं का समाधान आसानी से कर सकते हैं।",
+"संवाद और सहयोग समस्याओं का समाधान आसानी से कर सकते हैं।",
+"संवाद और सहयोग समस्याओं का समाधान आसानी से कर सकते हैं।",
+"संवाद और सहयोग समस्याओं का समाधान आसानी से कर सकते हैं।",
+"संवाद और सहयोग समस्याओं का समाधान आसानी से कर सकते हैं।"
+]
+
+tts = TTS(
+        model_name = None,
+        model_path = "hi/fastpitch/best_model.pth",
+        config_path = "hi/fastpitch/config.json",
+        vocoder_path = "hi/hifigan/best_model.pth",
+        vocoder_config_path = "hi/hifigan/config.json",
+        gpu = False)
+
+for index,value in enumerate(text_arr):
+
+    # wav = tts.tts_to_file(text = "बच्चों की रेरणादायक कहानियां",
+    wav = tts.tts_to_file(value,
+        speaker = "female",
+        file_path = f"output_array_{index}.wav",)
+
+
