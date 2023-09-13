ce54254a187e4915102bbd8c357a126023588059
BishanSingh246 <112897152+BishanSingh246@users.noreply.github.com>
2023-09-13 15:01:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,6 @@ tts = TTS(
         vocoder_path = "hi/hifigan/best_model.pth",
         vocoder_config_path = "hi/hifigan/config.json",
         progress_bar = False,
-        # cs_api_model = "XTTS",
         gpu = False)
 
 for index,value in enumerate(text_arr):
